3156e45cdc1d60cd9b4d089af14f09d42f7b03f4
japar36 <jaiganesh.paramanantham@inter.ikea.com>
2026-04-13 22:20:11 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello from GKE demo app (no DB yet)!');
+});
+
+app.get('/healthz', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
